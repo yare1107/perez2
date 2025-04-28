cbdc5f6767ab7eab2676b96e2f9d8cf254152fa6
Yare <sant0711yt@gmail.com>
2025-04-28 13:06:47 -0500

    Update rg-perfil.js

--- plugins/rg-perfil.js
+++ plugins/rg-perfil.js
@@ -1,26 +1,35 @@
-import { canLevelUp, xpRange } from '../lib/levelling.js'
+import { xpRange } from '../lib/levelling.js'
+import { createHash } from 'crypto'
 import PhoneNumber from 'awesome-phonenumber'
+import fetch from 'node-fetch'
 
-let handler = async (m, { conn, usedPrefix, command}) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   let user = global.db.data.users[who]
-  let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './storage/img/avatar_contact.png')
-  let { exp, limit, name, registered, regTime, age, level } = global.db.data.users[who]
+
+  // Verificamos si el usuario está registrado en la base de datos
+  if (!user) {
+    user = { exp: 0, limit: 0, name: 'Desconocido', level: 0 }
+  }
+
+  let { exp, limit, name, level } = user
   let { min, xp, max } = xpRange(user.level, global.multiplier)
-  let username = conn.getName(who)
-  let prem = global.prems.includes(who.split`@`[0])
+  let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://cdn.russellxz.click/801a44ad.jpeg')
+  let img = await (await fetch(`${pp}`)).buffer()
+
   let txt = `👤「 *User Perfil* 」⬣\n`
-     txt += `*🪴 Nombre ∙* ${name}\n`
-     txt += `*🐢 Edad ∙* ${age} años\n`
-     
-     txt += `*📞 Numero ∙* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}\n`
-     txt += `*🍬 Dulces ∙* ${limit}\n`
-     txt += `*💫 Experiencia ∙* Total ${exp} ( *${user.exp - min}/${xp}* )\n`
-     txt += `*👑 Premium ∙* ${prem ? 'Si' : 'No'}\n`
-     txt += `🚩`
-await conn.sendFile(m.chat, pp, 'thumbnail.jpg', txt, m)
+  txt += `*🪴 Nombre ∙* ${name}\n`
+  txt += `*📞 Numero ∙* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}\n`
+  txt += `*🍬 Dulces ∙* ${limit}\n`
+  txt += `*💫 Experiencia ∙* Total ${exp} ( *${user.exp - min}/${xp}* )\n`
+  txt += `*🚩 Nivel ∙* ${level}`
+
+  let mentionedJid = [who]
+  await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m)
 }
-handler.help = ['perfil', 'perfil @user']
-handler.tags = ['rg']
-handler.command = ['perfil', 'profile']
+
+handler.help = ['perfil', 'perfil *@user*']
+handler.tags = ['start']
+handler.command = /^(perfil|profile)$/i
+
 export default handler

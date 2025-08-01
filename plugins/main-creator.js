814c92f05244a88cfe80f84bba54ce2d9cc01462
Yare <sant0711yt@gmail.com>
2025-08-01 11:46:49 -0500

    perez2

--- plugins/main-creator.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;perez 👑;\nFN:perez 👑\nORG:perez 👑\nTITLE:\nitem1.TEL;waid=5219162788919:5219162788919\nitem1.X-ABLabel:perez 👑\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:perez 👑\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'おperez ⁩', contacts: [{ vcard }] }}, {quoted: m})
-}
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño'] 
-
-export default handler
--- tmp/sxnt.php
+++
@@ -1,1 +0,0 @@
-Deja los 2 archivos ya que es necesario para la limpieza
